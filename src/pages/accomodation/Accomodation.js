2f21eff19bc77aac80e6753fc2ec427c566e1fe7
cisseami <116657097+cisseami@users.noreply.github.com>
2023-03-10 00:57:34 +0100

    ajust accomodation

--- src/pages/accomodation/Accomodation.js
+++ src/pages/accomodation/Accomodation.js
@@ -21,8 +21,8 @@ export default function Housing() {
     const monObjet = accomodationId[0]
 
     useEffect(() => {
-        const monObjet = Datas.filter(data => data.id === identity);
-        setImageSlider(monObjet.pictures);
+        const accomodationId = Datas.filter(data => data.id === identity);
+        setImageSlider(accomodationId[0].pictures);
     }, [identity]);
 
 
@@ -36,8 +36,8 @@ export default function Housing() {
             <Carrousel imageSlider={imageSlider} />
             <GenericInfo accomodationId={monObjet} />
             <Collapse title={'Description'} content={description} />
-            <Collapse title={'Équipements'} content={equipments.map((eq, id) => (
-                <li className={styles.li} key={id}>{eq}</li>
+            <Collapse title={'Équipements'} content={equipments.map((eq, spe) => (
+                <li className={styles.li} key={spe}>{eq}</li>
             ))} />
         </>
     );
