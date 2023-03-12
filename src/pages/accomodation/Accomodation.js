376e0a15a3a9cd2fdfc866667fb42c9e4584a54e
cisseami <116657097+cisseami@users.noreply.github.com>
2023-03-12 16:33:59 +0100

    ajust2

--- src/pages/accomodation/Accomodation.js
+++ src/pages/accomodation/Accomodation.js
@@ -27,8 +27,8 @@ export default function Housing() {
     useEffect(() => {
         const accomodationId = Datas.filter(data => data.id === identity);
         setImageSlider(monObjet !== undefined ? accomodationId[0].pictures : null);
-        // eslint-disable-next-line
-    }, []);
+        
+    }, [identity, monObjet]);
 
 
 
